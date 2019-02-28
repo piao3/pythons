3edd336a0bbab199bfbc61f2ec29dd4bfa2dc419
piao3 <piaomingjie@beyondcent.com>
2019-02-28 16:15:25 +0800

    发射子弹

--- et-fighting.py
+++ et-fighting.py
@@ -2,6 +2,7 @@ import pygame
 from setting import Setting
 from ship import Ship
 import game_functions as gf
+from pygame.sprite import Group
 
 def run_game():
   ai_setting = Setting()
@@ -9,10 +10,15 @@ def run_game():
   screen = pygame.display.set_mode((ai_setting.screen_width, ai_setting.screen_height))
   pygame.display.set_caption("ET Fighting")
   ship = Ship(ai_setting, screen)
+  bullets = Group()
   
   while True:
-    gf.check_events(ship)
+    gf.check_events(ai_setting, screen, ship, bullets)
     ship.update()
-    gf.update_screen(ai_setting, screen, ship)
+    bullets.update()
+    for bullet in bullets.copy():
+      if bullet.rect.bottom <= 0:
+        bullets.remove(bullet)
+    gf.update_screen(ai_setting, screen, ship, bullets)
     
 run_game()
--- game_functions.py
+++ game_functions.py
@@ -1,22 +1,34 @@
 import pygame
 import sys
+from bullet import  Bullet
 
-def check_events(ship):
+def key_down(event, ai_setting, screen, ship, bullets):
+  if event.key == pygame.K_RIGHT:
+    ship.moving_right = True
+  if event.key == pygame.K_LEFT:
+    ship.moving_left = True
+  if event.key == pygame.K_SPACE:
+    new_bullet = Bullet(ai_setting, screen, ship)
+    bullets.add(new_bullet)
+    
+def key_up(event, ship):
+  if event.key == pygame.K_RIGHT:
+    ship.moving_right = False
+  if event.key == pygame.K_LEFT:
+    ship.moving_left = False
+  
+def check_events(ai_setting, screen, ship, bullets):
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       sys.exit()
     elif event.type == pygame.KEYDOWN:
-      if event.key == pygame.K_RIGHT:
-        ship.moving_right = True
-      if event.key == pygame.K_LEFT:
-        ship.moving_left = True
+      key_down(event, ai_setting, screen, ship, bullets)
     elif event.type == pygame.KEYUP:
-      if event.key == pygame.K_RIGHT:
-        ship.moving_right = False
-      if event.key == pygame.K_LEFT:
-        ship.moving_left = False
+      key_up(event, ship)
 
-def update_screen(ai_settings, screen, ship):
+def update_screen(ai_settings, screen, ship, bullets):
   screen.fill(ai_settings.bg_color)
+  for bullet in bullets.sprites():
+    bullet.draw_bullet()
   ship.blitme()
   pygame.display.flip()
